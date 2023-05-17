d3d70c6c3cf34d7f2b5d521b4963c1d7d42f59d0
johertrich <jhertrich@arcor.de>
2023-05-17 17:07:21 +0200

    add FashionMNIST and update MNIST

--- run_FashionMNIST.py
+++ run_FashionMNIST.py
@@ -8,23 +8,36 @@
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from utils.MMD_1D_der import *
+from MMD_1D_der import *
 import torchvision.datasets as td
 from torchvision.utils import make_grid
 from torchvision.transforms import transforms
 from torch.utils.data import DataLoader
 import pickle
 import copy
-from utils.unet import UNet
+from unet import UNet
 
 device='cuda'
 dtype=torch.float
-dir_name='MNIST_results'
+dir_name='FashionMNIST_results'
 
 if not os.path.isdir(dir_name):
     os.mkdir(dir_name)
 if not os.path.isdir(dir_name+'/nets'):
     os.mkdir(dir_name+'/nets')
+
+
+class scaled_UNet(torch.nn.Module):
+    # unet with normalized data
+    def __init__(self,unet,mean,std):
+        super(scaled_UNet,self).__init__()
+        self.mean=torch.nn.Parameter(mean,requires_grad=False)
+        self.std=torch.nn.Parameter(std,requires_grad=False)
+        self.unet=unet
+        self.add_module("unet",self.unet)
+
+    def forward(self,xs):
+        return self.std*self.unet(xs)+self.mean
 
 def get_UNET():
     # use unet
@@ -32,34 +45,38 @@ def get_UNET():
         input_channels=1,
         input_height=28,
         ch=32,
-        ch_mult=(1, 2, 2),
+        ch_mult=(1, 2, 4),
         num_res_blocks=2,
         attn_resolutions=(256,),
         resamp_with_conv=True,).to(device) 
 
+def get_scaled_unet(mean,std):
+    unet=get_UNET()
+    return scaled_UNet(unet,mean,std)
 
 def save_image(trajectory,name,nrow=25):
     grid = make_grid(trajectory,nrow=nrow,padding=1,pad_value=.5)
     plt.imsave(f'{dir_name}/{name}.png',torch.clip(grid.permute(1,2,0),0,1).cpu().numpy())
     return
     
-# Load target samples 
-mnist = td.MNIST('mnist',transform=transforms.ToTensor(),download=True)
+# Load target samples
+mnist = td.FashionMNIST('fmnist',transform=transforms.ToTensor(),download=True)
 M=20000
 data = DataLoader(dataset=mnist,batch_size=M)
 y = next(iter(data))[0].view(M,28**2).to(device)
 step_size=1.
-momentum=0.
-pickle.dump(y.detach().cpu().numpy(),open('imgs5/target.pickle',"wb"))
+pickle.dump(y.detach().cpu().numpy(),open(f'{dir_name}/target.pickle',"wb"))
+
 
 # Set parameters
 N=M
-momentum = 0.85
+momentum = 0.
 d=y.shape[-1]
 s_factor=sliced_factor(d)
-n_projections=1200
-opt_steps=500
-train_steps = 3001
+n_projections=1000
+proj_batches=1
+opt_steps=50
+train_steps=2001
 batch_size=100
 
 # initialize variables
@@ -69,56 +86,71 @@ trajectory = torch.empty(0,device=device)
 tic=time.time()
 x=torch.rand((N,1,28,28),dtype=dtype,device=device).reshape(N,-1)
 x_test=torch.rand((100,1,28,28),dtype=dtype,device=device)
-new_net=get_UNET()
+new_net=get_scaled_unet(torch.zeros((1,1,28,28),dtype=dtype,device=device),torch.ones((1,1,28,28),dtype=dtype,device=device))
 old_grad = torch.zeros((N,d), device = device)
 net_num=0
-first = True
 while True:
     print('Start',step,time.time()-tic)    
     x_old=torch.clone(x)
     print('Samples computed',step,time.time()-tic)
     # Compute the steps of the flow
     for _ in range(opt_steps):
-        xi=torch.randn((n_projections,d),dtype=dtype,device=device)
-        xi=xi/torch.sqrt(torch.sum(xi**2,-1,keepdim=True))
-        xi=xi.unsqueeze(1)
-        x_proj=torch.nn.functional.conv1d(x.reshape(1,1,-1),xi,stride=d).reshape(n_projections,-1)
-        y_proj=torch.nn.functional.conv1d(y.reshape(1,1,-1),xi,stride=d).reshape(n_projections,-1)
-        grad=MMD_derivative_1d(x_proj,y_proj)
-     
-        grad = grad.transpose(0,1)
-        xi = xi.reshape([n_projections,d]).transpose(0,1).flatten()
-        MMD_grad = s_factor* torch.nn.functional.conv1d(xi.reshape([1,1,-1]), grad.unsqueeze(1),stride=n_projections).squeeze()/n_projections   
+        MMD_grad=0.
+        for _ in range(proj_batches):
+            xi=torch.randn((n_projections,d),dtype=dtype,device=device)
+            xi=xi/torch.sqrt(torch.sum(xi**2,-1,keepdim=True))
+            xi=xi.unsqueeze(1)
+            x_proj=torch.nn.functional.conv1d(x.reshape(1,1,-1),xi,stride=d).reshape(n_projections,-1)
+            y_proj=torch.nn.functional.conv1d(y.reshape(1,1,-1),xi,stride=d).reshape(n_projections,-1)
+            grad=MMD_derivative_1d(x_proj,y_proj)
+         
+            grad = grad.transpose(0,1)
+            xi = xi.reshape([n_projections,d]).transpose(0,1).flatten()
+            MMD_grad = s_factor* torch.nn.functional.conv1d(xi.reshape([1,1,-1]), grad.unsqueeze(1),stride=n_projections).squeeze()/n_projections   + MMD_grad
 
-        MMD_grad = MMD_grad + momentum*old_grad
+        MMD_grad = MMD_grad/proj_batches + momentum*old_grad
         x=x-step_size*N*MMD_grad
         old_grad = MMD_grad
         if (step+1)%2000==0:
             print('Compute flow',step,time.time()-tic)
         step=step+1
+        #print((MMD_grad**2).sum().item())
 
     print('Train network',step,time.time()-tic)
+
     # difference to approximate
     many_grad=(x_old-x).view(-1,1,28,28)
+
+    # normalize data to approximate by neural network
+    mean=torch.mean(many_grad,0,keepdim=True)
+    std=torch.std(many_grad,0,keepdim=True)+1e-5
+    new_net.mean.data=torch.clone(mean.detach())
+    new_net.std.data=torch.clone(std.detach())
+    many_grad=(many_grad-mean)/std
+
+
     # train network Phi_l
     optim = torch.optim.Adam(new_net.parameters(), lr=0.001)
     loss_sum=0.
+    losses_ep=[]
     for ts in range(train_steps):
         perm=torch.randperm(many_grad.shape[0])[:batch_size]
         y_in=many_grad[perm]
         x_in=x_old[perm].view(batch_size,1,28,28)
-        loss=torch.sum((new_net(x_in)-y_in)**2)/batch_size
+        loss=torch.sum(torch.abs(new_net.unet(x_in)-y_in))/batch_size
         optim.zero_grad()
         loss.backward()
         optim.step()
         loss_sum+=loss.item()
+        losses_ep.append(loss.item())
         if (ts+1)%100==0:
-            print(ts+1,train_steps,loss.item(),loss_sum/(ts+1))
-    torch.save(new_net.state_dict(),'imgs5/nets/net'+str(net_num)+'.pt')
+            print(ts+1,train_steps,loss.item(),np.mean(losses_ep),loss_sum/(ts+1))
+            losses_ep=[]
+    torch.save(new_net.state_dict(),f'{dir_name}/nets/net'+str(net_num)+'.pt')
     net_num+=1
-    x_old=x_old.reshape(N,1,28,28)
 
     # Compute samples for the next network
+    x_old=x_old.reshape(N,1,28,28)
     with torch.no_grad():
         x_new=[]
         i=0
@@ -133,9 +165,18 @@ while True:
     save_image(x_test,'mnist_test'+str(step),nrow=10)
     x=x_new.reshape(N,-1).detach()
     print('Network trained',step,time.time()-tic)
-    pickle.dump(x.detach().cpu().numpy(),open('imgs5/samples.pickle',"wb"))
+    pickle.dump(x.detach().cpu().numpy(),open(f'{dir_name}/samples.pickle',"wb"))
+    pickle.dump(old_grad.detach().cpu().numpy(),open(f'{dir_name}/old_grad.pickle',"wb"))
+
     # update parameters
-    opt_steps= min(1000+opt_steps,30000)
+    if step<2000:
+        opt_steps=opt_steps+200
+    elif step<20000:
+        opt_steps=opt_steps+1000
+    else:
+        opt_steps=opt_steps+2000
+    opt_steps=min(opt_steps,15000)
+    momentum=min(momentum+0.05,0.8)
     if step>=4000001:
         break
 
